d9e77ab2212cac0c3434008f25452b9ee885a7ad
Mohan Ashapu <ashapumohan123@gmail.com>
2025-08-04 17:13:33 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/edumatrix");
+    await mongoose.connect("mongodb+srv://ashapumohan123:Mohan%401981@mohan.i5gtq.mongodb.net/vehicleaid?retryWrites=true&w=majority/edumatrix");
     console.log('✅ MongoDB connected');
   } catch (err) {
     console.error('❌ MongoDB connection error:', err);
